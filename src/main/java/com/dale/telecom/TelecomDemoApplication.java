b2e32381bb5247149f6a1147ab928c98c15d796e
Dale Waterworth <dale.waterworth@gmail.com>
2019-06-27 14:09:31 +0530

    Added swagger

--- src/main/java/com/dale/telecom/TelecomDemoApplication.java
+++ src/main/java/com/dale/telecom/TelecomDemoApplication.java
@@ -3,7 +3,10 @@ package com.dale.telecom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class TelecomDemoApplication {
 
 	public static void main(String[] args) {
